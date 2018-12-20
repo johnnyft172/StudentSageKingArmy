2388d500fbfc171a88536fa2610484427a23f5d7
johnnyft172 <45769359+johnnyft172@users.noreply.github.com>
2018-12-20 13:41:29 -0500

    Update Student.java

--- src/ArchType/Student.java
+++ src/ArchType/Student.java
@@ -2,7 +2,7 @@ package ArchType;
 
 public class Student {
     public static void main(String[] args) {
-        System.out.println("A dedicated student/learner must start with humility: I don't know, beginner mind, study" +
+        System.out.println("A dedicated student/learner must start with humility: I don't know, beginner's mind, study" +
                 "hard every single day therefore make progress one step at a a time");
     }
 }
